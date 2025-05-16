7ea4ca5d2b7c81e2b2ec357eb2ecacd76d555d9f
Rasel Miah <rasel6041@gmail.com>
2025-05-16 23:33:36 +0100

    pagination

---
+++ app/(mainLayout)/law-and-justice/AllLawAndJusticeArticles.tsx
@@ -0,0 +1,62 @@
+import { prisma } from "@/app/utils/db";
+import { EmptyState } from "../../../components/general/EmptyState";
+import { NewsArticleCard } from "../../../components/general/NewsArticleCard";
+
+async function getAllLawAndJusticeArticles() {
+  const [data] = await Promise.all([
+    prisma.newsArticle.findMany({
+      where: { newsCategory: "LAW_AND_JUSTICE" },
+      select: {
+        id: true,
+        createdAt: true,
+        isFeatured: true,
+        newsCategory: true,
+        newsDetails: true,
+        newsHeading: true,
+        newsPicture: true,
+        quotes: {
+          select: {
+            speakerInfo: true,
+            text: true,
+          },
+        },
+        newsResource: true,
+        newsPictureHeading: true,
+        newsPictureCredit: true,
+        newsLocation: true,
+        newsReporter: true,
+        newsArticleStatus: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    }),
+  ]);
+
+  return {
+    articles: data,
+  };
+}
+
+export default async function AllLawAndJusticeArticles() {
+  const { articles } = await getAllLawAndJusticeArticles();
+
+  return (
+    <>
+      {articles.length > 0 ? (
+        <div className="flex flex-col gap-6">
+          {articles.map((article, index) => (
+            <NewsArticleCard article={article} key={index} />
+          ))}
+        </div>
+      ) : (
+        <EmptyState
+          title="উফ! এখনো কিছু দেখানোর মতো নেই।"
+          description="এখনো কিছুই যুক্ত হয়নি। চোখ রাখুন!"
+          buttonText="প্রথম পৃষ্ঠায় যেতে ক্লিক করুন"
+          href="/"
+        />
+      )}
+    </>
+  );
+}
--- app/(mainLayout)/law-and-justice/page.tsx
+++ app/(mainLayout)/law-and-justice/page.tsx
@@ -1,9 +1,10 @@
 import AllNewsArticleList from "@/app/(mainLayout)/latest/AllNewsArticleList";
+import AllLawAndJusticeArticles from "./AllLawAndJusticeArticles";
 
 export default function LawAndJustice() {
     return (
       <div className="grid grid-cols-3 mt-10">
-        <div className="col-span-3 md:col-span-1"><AllNewsArticleList /></div>
+        <div className="col-span-3 md:col-span-1"><AllLawAndJusticeArticles /></div>
         <div className="col-span-3 md:col-span-1">three</div>
         <div className="col-span-3 md:col-span-1">one</div>
       </div>
