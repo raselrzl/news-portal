ec69a8e5b4e7c3d3c148deeac61b2634e3bccf21
Rasel Miah <rasel6041@gmail.com>
2025-05-16 23:38:26 +0100

    pagination

--- app/(mainLayout)/politics/page.tsx
+++ app/(mainLayout)/politics/page.tsx
@@ -1,9 +1,10 @@
 import AllNewsArticleList from "@/app/(mainLayout)/latest/AllNewsArticleList";
+import AllPoliticalArticles from "./AllPoliticalArticles";
 
 export default function Politics() {
     return (
       <div className="grid grid-cols-3 mt-10">
-        <div className="col-span-3 md:col-span-1"><AllNewsArticleList /></div>
+        <div className="col-span-3 md:col-span-1"><AllPoliticalArticles /></div>
         <div className="col-span-3 md:col-span-1">three</div>
         <div className="col-span-3 md:col-span-1">one</div>
       </div>
