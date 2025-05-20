3bf6580309ab7d57ee1080765ad2cf71368a5431
Rasel Miah <rasel6041@gmail.com>
2025-05-20 23:01:10 +0100

    added advertising form to database

--- app/(mainLayout)/post-an-article/post-advertisement/page.tsx
+++ app/(mainLayout)/post-an-article/post-advertisement/page.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { CreateAdvertisementForm } from "./post-advertisement-form";
 import { requireSuperAdmin } from "@/app/utils/requireUser";
-export default async function PostAnArticle() { 
-    const superadmin= await requireSuperAdmin()
+import { redirect } from "next/navigation";
+export default async function PostAnArticle() {
+  const approvedreporter = await requireSuperAdmin();
+  if (!approvedreporter) {
+    return redirect("/restricted");
+  }
   return (
     <div className="max-w-7xl mx-auto px-2 md:px-1 mb-10">
-        <div className="py-10">
-          <CreateAdvertisementForm
-          />
-        </div>
+      <div className="py-10">
+        <CreateAdvertisementForm />
+      </div>
     </div>
   );
 }
