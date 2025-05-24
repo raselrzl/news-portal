467fc240710fabc92317350024704279fc5ba0f3
Rasel Miah <rasel6041@gmail.com>
2025-05-24 19:03:25 +0100

    environment

--- app/(mainLayout)/environment/AllEnvironmentArticles.tsx
+++ app/(mainLayout)/environment/AllEnvironmentArticles.tsx
@@ -1,11 +1,16 @@
 import { prisma } from "@/app/utils/db";
 import { EmptyState } from "../../../components/general/EmptyState";
 import { NewsArticleCard } from "../../../components/general/NewsArticleCard";
+import { PaginationComponent } from "@/components/general/PaginationComponent";
 
-async function getAllEnvironmentArticles() {
-  const [data] = await Promise.all([
+async function getAllEnvironmentArticles(page: number = 1, pageSize: number = 8) {
+  const skip = (page - 1) * pageSize;
+
+  const [data, totalCount] = await Promise.all([
     prisma.newsArticle.findMany({
       where: { newsCategory: "ENVIRONMENT" },
+      take: pageSize,
+      skip,
       select: {
         id: true,
         createdAt: true,
@@ -31,15 +36,23 @@ async function getAllEnvironmentArticles() {
         createdAt: "desc",
       },
     }),
+    prisma.newsArticle.count({
+      where: { newsCategory: "ENVIRONMENT" },
+    }),
   ]);
 
   return {
     articles: data,
+    totalPages: Math.ceil(totalCount / pageSize),
   };
 }
 
-export default async function AllEnvironmentArticles() {
-  const { articles } = await getAllEnvironmentArticles();
+export default async function AllEnvironmentArticles({
+  currentPage,
+}: {
+  currentPage: number;
+}) {
+  const { articles, totalPages } = await getAllEnvironmentArticles(currentPage);
 
   return (
     <>
@@ -57,6 +70,7 @@ export default async function AllEnvironmentArticles() {
           href="/"
         />
       )}
+      <PaginationComponent totalPages={totalPages} currentPage={currentPage} />
     </>
   );
 }
