586d22eb110b9054b3039996d9ccbab91f8abf41
Rasel Miah <rasel6041@gmail.com>
2025-05-24 19:41:55 +0100

    opinion

--- app/(mainLayout)/science/page.tsx
+++ app/(mainLayout)/science/page.tsx
@@ -1,16 +1,31 @@
-import AllNewsArticleList from "@/app/(mainLayout)/latest/AllNewsArticleList";
 import AllScienceArticles from "./AllScienceArticles";
 import AllArticleList from "@/components/general/homepageArticleList";
+import LoadingSpinner from "@/components/general/LoadingSpinner";
+import { Suspense } from "react";
 
-export default function Science() {
+type SearchParamsProps = {
+  searchParams: Promise<{
+    page?: string;
+    jobTypes?: string;
+    location?: string;
+  }>;
+};
+
+export default async function Science({ searchParams }: SearchParamsProps) {
+  const params = await searchParams;
+  const currentPage = Number(params.page) || 1;
+
   return (
     <div className="grid grid-cols-3 mt-10">
       <div className="col-span-3 md:col-span-1">
         <h1 className="font-extrabold pl-2 mb-2">
-          {`>>>`}
-          বিজ্ঞান সংবাদ
+          {`>>>`}বিজ্ঞান সংবাদ
         </h1>
-        <AllScienceArticles />
+        <div className="p-1 md:p-4">
+          <Suspense key={currentPage} fallback={<LoadingSpinner />}>
+            <AllScienceArticles currentPage={currentPage} />
+          </Suspense>
+        </div>
       </div>
       <div className="col-span-3 md:col-span-2">
         <AllArticleList />
