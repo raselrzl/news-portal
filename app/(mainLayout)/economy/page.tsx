650bf6f26f3a65d6de2bcf721df73dd3410b99ab
Rasel Miah <rasel6041@gmail.com>
2025-05-24 18:57:09 +0100

    economy

--- app/(mainLayout)/economy/page.tsx
+++ app/(mainLayout)/economy/page.tsx
@@ -1,15 +1,31 @@
-import AllNewsArticleList from "@/app/(mainLayout)/latest/AllNewsArticleList";
-import AllEconomyArticles from "./AllEconomyArticles";
+import LoadingSpinner from "@/components/general/LoadingSpinner";
 import AllArticleList from "@/components/general/homepageArticleList";
+import AllEconomyArticles from "./AllEconomyArticles";
+import { Suspense } from "react";
 
-export default function Economy() {
+type SearchParamsProps = {
+  searchParams: Promise<{
+    page?: string;
+    jobTypes?: string;
+    location?: string;
+  }>;
+};
+
+export default async function Economy({ searchParams }: SearchParamsProps) {
+  const params = await searchParams;
+  const currentPage = Number(params.page) || 1;
+
   return (
     <div className="grid grid-cols-3 mt-10">
       <div className="col-span-3 md:col-span-1">
         <h1 className="font-extrabold pl-2 mb-2">
-          {`>>>`}সর্বশেষ সকল অর্থনীতির সংবাদ
+          {`>>>`} সর্বশেষ সকল অর্থনীতির সংবাদ
         </h1>
-        <AllEconomyArticles />
+        <div className="p-1 md:p-4">
+          <Suspense key={currentPage} fallback={<LoadingSpinner />}>
+            <AllEconomyArticles currentPage={currentPage} />
+          </Suspense>
+        </div>
       </div>
       <div className="col-span-3 md:col-span-2">
         <AllArticleList />
