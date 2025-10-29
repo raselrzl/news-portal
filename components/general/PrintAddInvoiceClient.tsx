a658e11f5fe1ed720e6f192eda8004ba7af3f330
raselrzl <rasel6041@gmail.com>
2025-10-29 11:03:55 +0100

    added district

--- components/general/PrintAddInvoiceClient.tsx
+++ components/general/PrintAddInvoiceClient.tsx
@@ -67,7 +67,7 @@ export default function PrintInvoiceClient({
 
   // ----- Assets & refs -----
   const filename = `INV-${ad.id.slice(0, 6).toUpperCase()}.pdf`;
-  const logoUrl = "/logoo1.jpg";
+  const logoUrl = "/lg1.png";
   const bannerUrl = ad.advertiseBanner || "";
 
   const contentRef = useRef<HTMLDivElement>(null);
