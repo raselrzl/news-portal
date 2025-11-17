b0ead9dfc218a07fa01b10cadf8bf913b1fb43c0
raselrzl <rasel6041@gmail.com>
2025-11-18 00:41:39 +0100

    uploaded documents and view

--- app/(mainLayout)/layout.tsx
+++ app/(mainLayout)/layout.tsx
@@ -14,7 +14,7 @@ export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div className="max-w-7xl mx-auto">
       <div className="fixed top-0 left-0 right-0 z-50 bg-background shadow-md max-w-7xl mx-auto">
-        <AdsTicker />
+   {/*      <AdsTicker /> */}
         <Navbar />
         <div className="flex justify-between items-center py-1 bg-primary-foreground">
           <Link
