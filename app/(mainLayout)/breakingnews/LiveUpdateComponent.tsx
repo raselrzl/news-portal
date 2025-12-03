670278e89a61c4f4139e16ab0c644b41c0fd35ae
raselrzl <rasel6041@gmail.com>
2025-12-03 11:20:49 +0100

    created news view option

--- app/(mainLayout)/breakingnews/LiveUpdateComponent.tsx
+++ app/(mainLayout)/breakingnews/LiveUpdateComponent.tsx
@@ -68,11 +68,12 @@ export default async function LiveUpdateComponent({
   const { news, totalPages } = await getPaginatedNews(currentPage, pageSize);
 
   return (
-    <div className="px-6 py-3 bg-red-50 dark:bg-black rounded-md mx-auto max-w-7xl">
-      <h1 className="text-sm uppercase font-bold mb-4 text-red-800">
-        🚨 ব্রেকিং নিউজ
+   <div className=" py-3 rounded-md mx-auto max-w-7xl">
+      <h1 className="uppercase font-bold mb-4 text-white text-xl ml-2 md:ml-70 py-2 bg-primary">
+          🚨 ব্রেকিং নিউজ
       </h1>
- {/* ========== FIRST NEWS FEATURE ========== */}
+
+      {/* ========== FIRST NEWS FEATURE ========== */}
       {news.length > 0 && (
         <div className="relative mb-14 flex flex-col items-center px-2">
           {news[0].newsPicture && (
